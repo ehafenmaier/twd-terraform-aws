62555f35b5ff7186c7d8541410d70acbb1e0a51f
Erich Hafenmaier <ehafenmaier@gmail.com>
2019-06-19 16:16:47 -0600

    Variables and outputs
    Added a variable for the server port and output for displaying the public ip address of the server.

--- aws-example-01/main.tf
+++ aws-example-01/main.tf
@@ -1,7 +1,17 @@
+# Provider
 provider "aws" {
   region = "us-east-1"
 }
 
+
+# Variables
+variable "server_port" {
+  description = "Port the server will use for HTTP requests"
+  default = 8080
+}
+
+
+# Resources
 resource "aws_instance" "example-01" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
@@ -9,7 +19,7 @@ resource "aws_instance" "example-01" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World..!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.server_port}" &
               EOF
 
   tags = {
@@ -21,9 +31,15 @@ resource "aws_security_group" "instance-01" {
   name = "terraform-example-instance-01"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.server_port}"
+    to_port     = "${var.server_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+
+# Outputs
+output "public_ip" {
+  value = "${aws_instance.example-01.public_ip}"
 }
