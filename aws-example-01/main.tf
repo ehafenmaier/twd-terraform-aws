2e459e91366bc7b2ab52287074a121acc5d5c04c
Erich Hafenmaier <ehafenmaier@gmail.com>
2019-06-19 15:44:40 -0600

    Adding aws security group & user data
    Added resource for aws security group to allow tcp traffic through port 8080. Added user data section to spin up busybox web server.

--- aws-example-01/main.tf
+++ aws-example-01/main.tf
@@ -3,10 +3,27 @@ provider "aws" {
 }
 
 resource "aws_instance" "example-01" {
-  ami = "ami-40d28157"
+  ami           = "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance-01.id}"]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World..!" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     Name = "terraform-example-01"
+  }
+}
+
+resource "aws_security_group" "instance-01" {
+  name = "terraform-example-instance-01"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
